12cd5f1df79b7084cbc23261b7ad94e37667264f
Sakshi Rai <121843928+sakshi1feb@users.noreply.github.com>
2024-03-19 13:47:48 +0000

    learned about let const and var keyword

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553 // The value of variable cant be changed later in the program.
+let accountEmail = "sakshi.google.com"//the value of variable can be changed later in the program
+var accountPassword = "2134"// not used now because the variable can be re-declared.
+accountCity = "Jaipur" // can be done is this form as well but not a good practice.
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
